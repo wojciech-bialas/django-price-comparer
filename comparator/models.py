7001e20a2c64a8895fdb6dc38e5af50d0e41ff47
Wojciech <wojciech.bialas@protonmail.com>
2022-04-02 13:04:11 +0200

    komputronik scraper

--- comparator/models.py
+++ comparator/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Product(models.Model):
-    code = models.CharField(max_length=50)
+    code = models.CharField(max_length=60)
     name = models.CharField(max_length=120)
 
 class ProductOffer(models.Model):
