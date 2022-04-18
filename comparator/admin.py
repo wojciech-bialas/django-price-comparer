6d322afeaf21183c1c43a2d6139f243b22b0bc52
Wojciech <breeze.dnb@gmail.com>
2022-04-18 20:22:07 +0200

    register model to admin panel

--- comparator/admin.py
+++ comparator/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Product, ProductOffer, ProductPrice, ObservedProducts
 
 # Register your models here.
+admin.site.register(Product)
+admin.site.register(ProductOffer)
+admin.site.register(ProductPrice)
+admin.site.register(ObservedProducts)
