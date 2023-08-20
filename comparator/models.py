bf5166227ac86e8b78cbd9ad10c7e2be7b2d897d
Wojciech Bialas <wojciech.bialas@protonmail.com>
2023-08-20 13:43:51 +0200

    enable deleting product with its related offers and prices

--- comparator/models.py
+++ comparator/models.py
@@ -11,12 +11,12 @@ class ProductOffer(models.Model):
     link = models.CharField(max_length=250)
     image = models.CharField(max_length=250)
     shop = models.CharField(max_length=20)
-    product = models.ForeignKey(Product, on_delete=models.PROTECT)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
 
 class ProductPrice(models.Model):
     price = models.FloatField()
     date = models.DateField()
-    product_offer = models.ForeignKey(ProductOffer, on_delete=models.PROTECT)
+    product_offer = models.ForeignKey(ProductOffer, on_delete=models.CASCADE)
 
 class ObservedProducts(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
